52f44d1e8aebc474dd87a3da5525ad469e19832d
Coding-Ayko <tauane581730@gmail.com>
2025-08-05 10:56:21 -0300

    Feat: Estrutura inicial Front App

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [projetos, setProjetos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5151/api/projetos")
+      .then((res) => res.json())
+      .then((data) => setProjetos(data))
+      .catch((err) => console.error("Erro API:", err));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h1>Meus Projetos</h1>
+      <ul>
+        {projetos.map((p) => (
+          <li key={p.id}>
+            <strong>{p.nome}</strong> — {p.descricao}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
